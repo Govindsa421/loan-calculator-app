3120b8a6bd7deade082b3dcd6b8736e0fcddde02
govind <govindsa421@gmail.com>
2025-01-28 17:17:33 +0530

    fix issues

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex h-screen justify-center items-center text-xl text-red-500">
+      Sorry, this page is currently under construction. Please check back later.
+    </div>
+  );
+};
+
+export default NotFoundPage;
